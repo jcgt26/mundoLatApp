0ef5a2c1aa332b97febde5abbb03a882037ed472
juancamiloguzmantoro <toro@lean-ocean.com>
2023-07-29 20:17:41 +0200

    Add repositories to services

--- src/main/java/com/mundo_latino/address/AddressService.java
+++ src/main/java/com/mundo_latino/address/AddressService.java
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddressService {
 
+    private final AddressRepository addressRepository;
+
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
 }
--- src/main/java/com/mundo_latino/location/LocationService.java
+++ src/main/java/com/mundo_latino/location/LocationService.java
@@ -4,4 +4,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LocationService {
+    private final LocationRepository locationRepository;
+
+    public LocationService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
 }
